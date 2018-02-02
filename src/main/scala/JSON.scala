fd0eb432a3737e90fd3e7c5027ce0a9265ec35d3
Pietro.Speri <speri.pietro@gmail.com>
2018-02-02 14:03:09 -0800

    Added value, obj, arr, member methods definition

--- src/main/scala/JSON.scala
+++ src/main/scala/JSON.scala
@@ -1,8 +1,12 @@
 import scala.util.parsing.combinator._
+import scala.util.parsing.combinator.JavaTokenParsers
 /**
   * Created by Pietro.Speri on 02/02/2018.
   */
 
 class JSON extends JavaTokenParsers {
-
+  def value:Parser[Any] = obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
+  def obj:Parser[Any]
+  def arr:Parser[Any]
+  def member:Parser[Any]
 }
