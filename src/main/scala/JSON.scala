cc43d1d6b259e1773e7b378bf5bd3a85680f1743
Pietro.Speri <speri.pietro@gmail.com>
2018-02-02 14:07:46 -0800

    Arr implementation

--- src/main/scala/JSON.scala
+++ src/main/scala/JSON.scala
@@ -7,6 +7,6 @@ import scala.util.parsing.combinator.JavaTokenParsers
 class JSON extends JavaTokenParsers {
   def value:Parser[Any] = obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
   def obj:Parser[Any] = "{"~repsep(member,",")~"}"
-  def arr:Parser[Any]
+  def arr:Parser[Any] = "["~repsep(value, ",")~"]"
   def member:Parser[Any]
 }
