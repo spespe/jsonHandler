85bb5780793a1cb19c5eb64b8506d72a03fa38f1
Pietro.Speri <speri.pietro@gmail.com>
2018-02-05 09:26:59 -0800

    Changed the member method to convert the result in a name-value tuple

--- src/main/scala/JSON.scala
+++ src/main/scala/JSON.scala
@@ -8,5 +8,5 @@ class JSON extends JavaTokenParsers {
   def value:Parser[Any] = obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
   def obj:Parser[Map[String,Any]] = "{"~>repsep(member,",")<~"}" ^^ (Map() ++ _)
   def arr:Parser[List[Any]] = "["~>repsep(value, ",")<~"]"
-  def member:Parser[(String,Any)] = stringLiteral~":"~value
+  def member:Parser[(String,Any)] = stringLiteral~":"~value ^^ {case name~":"~value => (name,value)}
 }
