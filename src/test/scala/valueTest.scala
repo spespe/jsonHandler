d4b1e3ee9de3f94d0d1c85e906843a02aa6ada6e
Pietro.Speri <speri.pietro@gmail.com>
2018-02-05 17:28:44 -0800

    Added for cycle for tests and results assertions
    Added definition of testing method

--- src/test/scala/valueTest.scala
+++ src/test/scala/valueTest.scala
@@ -1,16 +1,24 @@
 /**
   * Created by Pietro.Speri on 05/02/2018.
   */
+import jsonHandler.parse
 import org.scalatest.FunSuite
 import scala.language._
 import scala.xml.NodeSeq
 
 class valueTest(nd:NodeSeq) extends FunSuite {
-  for{
-    (el,idx)<-(nd \\ "@name").zipWithIndex
-  }{
+
+  var res,timestmp=""
+
+  def testing(input:String)()={
+    //to be finished
+    val res = parse(value,reader)
+      res
+  }
+
+  for(el<-(nd \\ "@name")){
     test(el.text){
-      //
+      // assert
     }
   }
 }
