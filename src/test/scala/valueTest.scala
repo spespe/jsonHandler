3577c62bba9c914b41520f39d0c09b71216ba514
Pietro.Speri <speri.pietro@gmail.com>
2018-02-07 12:18:53 -0800

    Added valueTest main for cycle for tests-results iteration
    To be modified with the parser addition

--- src/test/scala/valueTest.scala
+++ src/test/scala/valueTest.scala
@@ -1,15 +1,16 @@
 /**
   * Created by Pietro.Speri on 05/02/2018.
   */
-import JSONTest.elem
-import jsonHandler.parse
 import org.scalatest.FunSuite
-
 import scala.language._
-import scala.xml.{Node, NodeSeq}
+import scala.xml.{Node}
 
-class valueTest(s:Seq[((Node,Node),Int)]) extends FunSuite {
-
-  var res,timestmp=""
-
+class valueTest(seq:Seq[((Node,Node),Int)]) extends FunSuite {
+  for((el,idx)<-seq){
+    test(""){
+      val res = el._1
+      val expRes = el._2
+      assert(res==expRes)
+    }
+  }
 }
