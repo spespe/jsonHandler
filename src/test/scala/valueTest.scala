dc9ea2954cfb32d2c2103ed2e4fa4612cfc42870
Pietro.Speri <speri.pietro@gmail.com>
2018-02-07 12:31:09 -0800

    Added name and idx to the for cycle

--- src/test/scala/valueTest.scala
+++ src/test/scala/valueTest.scala
@@ -5,9 +5,9 @@ import org.scalatest.FunSuite
 import scala.language._
 import scala.xml.{Node}
 
-class valueTest(seq:Seq[((Node,Node),Int)]) extends FunSuite {
-  for((el,idx)<-seq){
-    test("value test number: "+idx){
+class valueTest(seq:Seq[(((Node,Node),Int),Node)]) extends FunSuite {
+  for(((el,idx),name)<-seq){
+    test("VALUE TEST NUMBER: "+idx+" NAME:"+name){
       val res = el._1
       val expRes = el._2
       assert(res==expRes)
