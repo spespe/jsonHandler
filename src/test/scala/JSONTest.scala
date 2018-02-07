2061085a7f840ac802d5d190ac4bd5df4ee13da8
Pietro.Speri <speri.pietro@gmail.com>
2018-02-07 11:56:11 -0800

    Changed seqCreator method to return Seq of Nodes and Int

--- src/test/scala/JSONTest.scala
+++ src/test/scala/JSONTest.scala
@@ -1,8 +1,6 @@
 import java.io.InputStream
-
 import org.scalatest.FunSuite
-
-import scala.xml.{NodeSeq, XML}
+import scala.xml.{NodeSeq, XML, Node}
 /**
   * Created by Pietro.Speri on 05/02/2018.
   */
@@ -10,20 +8,23 @@ object JSONTest extends FunSuite {
   var input: Option[InputStream] = None
   var elem: scala.xml.Elem = _
 
+  //Test sequence creator method
+  def seqCreator(nd:NodeSeq):Seq[((Node,Node),Int)]={
+    val test = (nd \\ "test")
+    val result = (nd \\ "result")
+    test.zip(result).zipWithIndex
+  }
+
   def main(args: Array[String]): Unit = {
+
     try {
       val input = Some(getClass.getResourceAsStream("/tests.xml"))
       elem = XML.load(input.get)
       val value = (elem \\ "unit").filter(i => i.attribute("tag").get.text == "value")
 
-      def seqCreator(nd:NodeSeq):Seq[((Any,Any),Int)]={
-        val test = (nd \\ "test").text.trim
-        val result = (nd \\ "result").text.trim
-        test.zip(result).zipWithIndex
-      }
-
-      //run
-
+    } catch {
+      case ex:Exception => ex.printStackTrace
+        ex.getMessage
     }
   }
 
