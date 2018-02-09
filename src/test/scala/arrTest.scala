6d3f85913d7c524b262f6dc3e337170055f5a8bd
Pietro.Speri <speri.pietro@gmail.com>
2018-02-08 16:03:22 -0800

    Added arrTest class

--- src/test/scala/arrTest.scala
+++ src/test/scala/arrTest.scala
@@ -1,6 +1,16 @@
+import org.scalatest.FunSuite
+
+import scala.xml.Node
+
 /**
   * Created by Pietro.Speri on 07/02/2018.
   */
-class arrTest {
-
+class arrTest (seq:Seq[(((Node,Node),Int),Node)]) extends FunSuite {
+  for(((el,idx),name)<-seq){
+    test("ARRAY TEST NUMBER: "+idx+" NAME:"+name){
+      val res = el._1
+      val expRes = el._2
+      assert(res==expRes)
+    }
+  }
 }
