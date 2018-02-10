055635d23269794ddfa1d934c2384c6c779da7be
Pietro.Speri <speri.pietro@gmail.com>
2018-02-09 17:01:28 -0800

    Added res val for objTest class

--- src/test/scala/objTest.scala
+++ src/test/scala/objTest.scala
@@ -8,7 +8,11 @@ import scala.xml.Node
 class objTest (seq:Seq[(((Node,Node),Int),Node)]) extends FunSuite {
   for(((el,idx),name)<-seq){
     test("OBJECT TEST NUMBER: "+idx+" NAME:"+name){
-      val res = el._1
+      val res = parse(obj, el._1) match {
+        case Success(matched _) => matched
+        case Failure(failMsg, _) => println("FAILURE: "+msg)
+        case Error(errMsg, _) => println("ERROR: "+errMsg)
+      }
       val expRes = el._2
       assert(res==expRes)
     }
