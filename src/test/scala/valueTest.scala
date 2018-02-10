a84468be1360631a9e82f4f9a3b8a5ce2881b271
Pietro.Speri <speri.pietro@gmail.com>
2018-02-09 17:00:34 -0800

    Added res val for valueTest class

--- src/test/scala/valueTest.scala
+++ src/test/scala/valueTest.scala
@@ -4,11 +4,16 @@
 import org.scalatest.FunSuite
 import scala.language._
 import scala.xml.{Node}
+import scala.util.parsing.combinator._
 
 class valueTest(seq:Seq[(((Node,Node),Int),Node)]) extends FunSuite {
   for(((el,idx),name)<-seq){
     test("VALUE TEST NUMBER: "+idx+" NAME:"+name){
-      val res = el._1
+      val res = parse(value, el._1) match {
+        case Success(matched _) => matched
+        case Failure(failMsg, _) => println("FAILURE: "+msg)
+        case Error(errMsg, _) => println("ERROR: "+errMsg)
+      }
       val expRes = el._2
       assert(res==expRes)
     }
