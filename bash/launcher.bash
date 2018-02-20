79a1b088411d67752c623c9f498d3b714f2a2737
Pietro.Speri <speri.pietro@gmail.com>
2018-02-20 14:58:14 -0800

    Added usage() method

--- bash/launcher.bash
+++ bash/launcher.bash
@@ -3,7 +3,16 @@
 
 ##Launcher
 usage(){
-	echo -e "\e[33m USAGE: java -jar jsonHandler.jar [OPTIONAL - parameters] \e[0m"
+	echo -e "\e[33m USAGE: ./launcher.sh [-][OPTIONAL PARAMETERS] \e[0m"
+	echo -e "\e[33m OPTION -f : allows to specify the file name to pass as input(JSON format file) \e[0m"
+	echo -e "\e[33m OPTION -m : allows to retrieve the members only from the JSON input file specified above \e[0m"
+	echo -e "\e[33m OPTION -a : allows to retrieve the arrays only from the JSON input file specified above \e[0m"
+	echo -e "\e[33m OPTION -o : allows to retrieve the objects only from the JSON input file specified above \e[0m"
+	echo -e "\e[32m EXAMPLE: ./launcher.sh -m /path/to/my/file/fille.json \e[0m"
+	
 }
 
 ##Parameters
+
+
+
