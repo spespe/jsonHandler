93380b16e9a1c53cf64c7a484eec63d99e54fe32
Pietro.Speri <speri.pietro@gmail.com>
2018-03-02 11:07:19 -0800

    Added reader via StreamReader
    Added case for value reader

--- src/main/scala/jsonHandler.scala
+++ src/main/scala/jsonHandler.scala
@@ -1,13 +1,16 @@
+import java.io.{FileInputStream, InputStreamReader}
 import scala.io.Source
 import java.util.Calendar
+
 import com.typesafe.scalalogging.LazyLogging
+
+import scala.util.parsing.input.StreamReader
 
 /**
   * Created by Pietro.Speri on 26/01/2018.
   */
 
 object jsonHandler extends JSON with ArgumentsParser with LazyLogging {
-
   def main(args:Array[String]){
       logger.info("{DATETIME}")
       println(Calendar.getInstance.getTime)
@@ -19,9 +22,13 @@ object jsonHandler extends JSON with ArgumentsParser with LazyLogging {
       argsParser(Map(),argsList).foreach(println)
 
       //File reader
-      val file = Source.fromFile("").getLines
-      val words = file.flatMap(_.split(" "))
-
+      val reader = StreamReader(new InputStreamReader(new FileInputStream("C:\\Users\\pietro.speri\\IdeaProjects\\jsonHandler\\src\\test\\resources\\jsonExample.json")))
+      //Case to be added to separate the options
+      parse(value,reader) match {
+          case Success(succMatch, _) => succMatch
+          case Failure(failMsg, _) => println("FAILED: "+failMsg)
+          case Error(errMsg, _) => println("ERROR: "+errMsg)
+      }
     }
 }
 
