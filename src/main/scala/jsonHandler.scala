1c78293b93cd32a97999f70f7df55f57b0c45796
Pietro.Speri <speri.pietro@gmail.com>
2018-03-05 11:20:29 -0800

    Added correct values for arguments

--- src/main/scala/jsonHandler.scala
+++ src/main/scala/jsonHandler.scala
@@ -17,15 +17,9 @@ object jsonHandler extends JSON with ArgumentsParser with LazyLogging {
       argsList.foreach(println)
 
       //File reader
-      val reader = StreamReader(new InputStreamReader(new FileInputStream("C:\\Users\\pietro.speri\\IdeaProjects\\jsonHandler\\src\\test\\resources\\jsonExample.json")))
+      //val reader = StreamReader(new InputStreamReader(new FileInputStream("")))
+      //"C:\\Users\\pietro.speri\\IdeaProjects\\jsonHandler\\src\\test\\resources\\jsonExample.json"
       //Case to be added to separate the options
-      argsList.map{ x =>
-          if(x._2.contains("-f"){}
-          if(x._2.contains("-o"){}
-          if(x._2.contains("-s"){}
-          if(x._2.contains("-p"){}
-          if(x._2.contains("-t"){}
-      }
 
       //parse(value,"0.9") match {
       //    case Success(succMatch, _) => println("SUCCESS: "+succMatch)
