f4183e13d92eb21f10058f5710333042c6d3a7cd
Pietro.Speri <speri.pietro@gmail.com>
2018-03-05 10:59:18 -0800

    Added new implementation idea

--- src/main/scala/jsonHandler.scala
+++ src/main/scala/jsonHandler.scala
@@ -1,9 +1,6 @@
 import java.io.{FileInputStream, InputStreamReader}
-import scala.io.Source
 import java.util.Calendar
-
 import com.typesafe.scalalogging.LazyLogging
-
 import scala.util.parsing.input.StreamReader
 
 /**
@@ -16,19 +13,40 @@ object jsonHandler extends JSON with ArgumentsParser with LazyLogging {
       println(Calendar.getInstance.getTime)
 
       logger.info("{CHECKING INPUT ARGUMENTS}")
-      val argsList = args.toList
-
-      logger.info("{PARAMETERS}")
-      argsParser(Map(),argsList).foreach(println)
+      val argsList = argsParser(Map(),args.toList)
+      argsList.foreach(println)
 
       //File reader
       val reader = StreamReader(new InputStreamReader(new FileInputStream("C:\\Users\\pietro.speri\\IdeaProjects\\jsonHandler\\src\\test\\resources\\jsonExample.json")))
       //Case to be added to separate the options
-      parse(value,reader) match {
-          case Success(succMatch, _) => succMatch
-          case Failure(failMsg, _) => println("FAILED: "+failMsg)
-          case Error(errMsg, _) => println("ERROR: "+errMsg)
+      argsList.map{ x =>
+          if(x._2.contains("-f"){}
+          if(x._2.contains("-o"){}
+          if(x._2.contains("-s"){}
+          if(x._2.contains("-p"){}
+          if(x._2.contains("-t"){}
       }
+
+      //parse(value,"0.9") match {
+      //    case Success(succMatch, _) => println("SUCCESS: "+succMatch)
+      //    case Failure(failMsg, _) => println("FAILED: "+failMsg)
+      //    case Error(errMsg, _) => println("ERROR: "+errMsg)
+      //}
+      //parse(obj,"") match {
+      //    case Success(succMatch, _) => println("SUCCESS: "+succMatch)
+      //    case Failure(failMsg, _) => println("FAILED: "+failMsg)
+      //    case Error(errMsg, _) => println("ERROR: "+errMsg)
+      //}
+      //parse(arr,"") match {
+      //    case Success(succMatch, _) => println("SUCCESS: "+succMatch)
+      //    case Failure(failMsg, _) => println("FAILED: "+failMsg)
+      //    case Error(errMsg, _) => println("ERROR: "+errMsg)
+      //}
+      //parse(member,"") match {
+      //    case Success(succMatch, _) => println("SUCCESS: "+succMatch)
+      //    case Failure(failMsg, _) => println("FAILED: "+failMsg)
+      //    case Error(errMsg, _) => println("ERROR: "+errMsg)
+      //}
     }
 }
 
