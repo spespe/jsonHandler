beb339fd78e2b5008f4306571114d87e9ee087e8
Pietro.Speri <speri.pietro@gmail.com>
2018-03-09 11:54:49 -0800

    Fixed utilT trait

--- src/main/scala/utilT.scala
+++ src/main/scala/utilT.scala
@@ -8,32 +8,30 @@ import scala.xml.{Node, NodeSeq, XML}
   * Created by Pietro.Speri on 07/03/2018.
   */
 
-        trait utilT {
+trait utilT {
 
-          def getTime=println(Calendar.getInstance.getTime)
+  def getTime=println(Calendar.getInstance.getTime)
+  @volatile var elem: scala.xml.Elem = _
+  val input = getClass.getResourceAsStream("/tests.xml")
+  try {
+      elem = XML.load(input)
+        } catch {
+          case ex:Throwable => System.err.println("[NOT ABLE TO LOAD THE XML FILE]")
+            ex.printStackTrace
+            System.exit(1)
+        }
 
-          var elem: scala.xml.Elem = _
-          //val input = Some(getClass.getResourceAsStream("test/resources/tests.xml"))
-          val input = new FileInputStream("testClasses/resources/tests.xml")
-          try {
-            elem = XML.load(input)
-          } catch {
-            case ex:Throwable => System.err.println("[NOT ABLE TO LOAD THE XML FILE]")
-              ex.printStackTrace
-              System.exit(1)
+          //Test sequence creator method
+          def seqCreator(nd:NodeSeq):Seq[(((Node,Node),Int),Node)]={
+            val test = (nd \\ "testClasses")
+            val result = (nd \\ "result")
+            val name = (nd \\ "@name")
+            test.zip(result).zipWithIndex.zip(name)
           }
-
-            //Test sequence creator method
-            def seqCreator(nd:NodeSeq):Seq[(((Node,Node),Int),Node)]={
-              val test = (nd \\ "testClasses")
-              val result = (nd \\ "result")
-              val name = (nd \\ "@name")
-              test.zip(result).zipWithIndex.zip(name)
-            }
-            def inputTestValidator(s:String):Option[NodeSeq]={
-              (elem \\ "unit").filter(i => i.attribute("tag").get.text == s) match {
-                case n:NodeSeq => Some(n)
-                case _ => println("INPUT NOT FOUND IN THE TESTS.\n"); None
-              }
+          def inputTestValidator(s:String):Option[NodeSeq]={
+            (elem \\ "unit").filter(i => i.attribute("tag").get.text == s) match {
+              case n:NodeSeq => Some(n)
+              case _ => println("INPUT NOT FOUND IN THE TESTS.\n"); None
             }
           }
+        }
