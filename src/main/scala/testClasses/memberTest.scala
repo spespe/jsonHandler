b0dc8c7c516b6451ad1bb34cc7d22ddd457730cf
Pietro.Speri <speri.pietro@gmail.com>
2018-03-09 17:05:18 -0800

    Added all the tests for the basic components

--- src/main/scala/testClasses/memberTest.scala
+++ src/main/scala/testClasses/memberTest.scala
@@ -1,5 +1,6 @@
 package testClasses
 
+import jsonHandler.JSONParser
 import org.scalatest.FunSuite
 
 import scala.xml.Node
@@ -7,16 +8,16 @@ import scala.xml.Node
 /**
   * Created by Pietro.Speri on 07/02/2018.
   */
-class memberTest (seq:Seq[(((Node,Node),Int),Node)]) extends FunSuite {
+class memberTest (seq:Seq[(((Node,Node),Int),Node)]) extends FunSuite with JSONParser{
   for(((el,idx),name)<-seq){
     test("MEMBER TEST NUMBER: "+idx+" NAME:"+name){
-      //val res = parse(member, el._1) match {
-      //  case Success(matched _) => matched
-      //  case Failure(failMsg, _) => println("FAILURE: "+msg)
-      //  case Error(errMsg, _) => println("ERROR: "+errMsg)
-      //}
-      //val expRes = el._2
-      //assert(res==expRes)
+      val res = parse(member, el._1.asInstanceOf[CharSequence]) match {
+        case Success(matched, _) => matched
+        case Failure(failMsg, _) => println("FAILURE: "+failMsg)
+        case Error(errMsg, _) => println("ERROR: "+errMsg)
+      }
+      val expRes = el._2
+      assert(res==expRes)
     }
   }
 }
