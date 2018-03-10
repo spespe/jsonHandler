ceeb0ab965ee87bffd1a6d41992cf7e60538a904
Pietro.Speri <speri.pietro@gmail.com>
2018-03-09 16:44:40 -0800

    Added first test for value

--- src/main/scala/testClasses/valueTest.scala
+++ src/main/scala/testClasses/valueTest.scala
@@ -10,14 +10,14 @@ import scala.xml.Node
 
 class valueTest(seq:Seq[(((Node,Node),Int),Node)]) extends FunSuite with JSONParser {
   for(((el,idx),name)<-seq){
-    //test("VALUE TEST NUMBER: "+idx+" NAME:"+name){
-      //val res = parse(value, el._1) match {
-      //  case Success(matched _) => matched
-      //  case Failure(failMsg, _) => println("FAILURE: "+msg)
-      //  case Error(errMsg, _) => println("ERROR: "+errMsg)
-      //}
-      //val expRes = el._2
-      //assert(res==expRes)
-  //  }
+    test("VALUE TEST NUMBER: "+idx+" NAME:"+name){
+      val res = parse(value, el._1.asInstanceOf[CharSequence]) match {
+        case Success(matched, _) => println(matched)
+        case Failure(failMsg, _) => println("FAILURE: "+failMsg)
+        case Error(errMsg, _) => println("ERROR: "+errMsg)
+      }
+      val expRes = el._2
+      assert(res==expRes)
+    }
   }
 }
