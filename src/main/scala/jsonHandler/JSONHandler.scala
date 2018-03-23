407965a7f89bd2c1d27200d75d70eb4ce0c62bf7
Pietro.Speri <speri.pietro@gmail.com>
2018-03-23 16:39:34 -0700

    Added input JSON file as first check

--- src/main/scala/jsonHandler/JSONHandler.scala
+++ src/main/scala/jsonHandler/JSONHandler.scala
@@ -24,31 +24,31 @@ object JSONHandler extends UtilT {
     }
 
     argsList match {
-    case x: JSONHandler.ParserMap if (x.contains('TestLauncher)) => //Launching Tests
-      if(argValidator(x)("TestLauncher")("y")) {
-        logger.debug("{LAUNCHING TESTS. PARAMETER PASSED: "+x.get('TestLauncher).get+"}")
+      case x: JSONHandler.ParserMap if (!x.contains('InputFile)) => {
+        usage
+        System.err.println("THE INPUT JSON FILE IS REQUIRED!")
+        System.exit(1)
+      }
+      case x: JSONHandler.ParserMap if (x.contains('TestLauncher)) => //Launching Tests
+        if(argValidator(x)("TestLauncher")("y")) {
+          logger.debug("{LAUNCHING TESTS. PARAMETER PASSED: "+x.get('TestLauncher).get+"}")
         try {
           elementList.map(s => (inputTestValidator(ns, s), s)).foreach(y => launchTest(y._1,y._2))
         } catch {
           case ex: Exception => ex.printStackTrace; ex.getMessage
         }
       } else {
-        logger.debug("{TESTS SKIPPED. PARAMETER PASSED: "+x.get('TestLauncher).get+"}")
+          logger.debug("{TESTS SKIPPED. PARAMETER PASSED: "+x.get('TestLauncher).get+"}")
       }
     case x: JSONHandler.ParserMap if (x.contains('ObjectParser)) => {
-      logger.info("{LAUNCHING JSON PARSER ON " + x('InputFile) + "USING " + x('ObjectParser) + "}")
+        logger.info("{LAUNCHING JSON PARSER ON " + x('InputFile) + "USING " + x('ObjectParser) + "}")
       parse(x('ObjectParser), x('InputFile))
     } // Launching for obj
-    case x: JSONHandler.ParserMap if (x.contains('Parallel)) => {
-      //the message will be replace with a new method
-      logger.info("{LAUNCHING JSON PARSER ON " + x('InputFile) + "USING " + x('ObjectParser) + "}")
-      //parse(x('Parallel),"")
-    }
-    case x: JSONHandler.ParserMap if (!x.contains('InputFile)) => {
-      usage
-      System.err.println("THE INPUT JSON FILE IS REQUIRED!")
-      System.exit(1)
-    }
+    //case x: JSONHandler.ParserMap if (x.contains('Parallel)) => {
+    //    //the message will be replace with a new method
+    //    logger.info("{LAUNCHING JSON PARSER ON " + x('InputFile) + "USING " + x('ObjectParser) + "}")
+    //  //parse(x('Parallel),"")
+    //}
     }
   }
 }
