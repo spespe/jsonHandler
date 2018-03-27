c74ef6c783d85edb37e05e879f04807ebc70b7bb
Pietro.Speri <speri.pietro@gmail.com>
2018-03-27 16:18:02 -0700

    Removed match case from JSONHandler

--- src/main/scala/jsonHandler/JSONHandler.scala
+++ src/main/scala/jsonHandler/JSONHandler.scala
@@ -5,35 +5,37 @@ package jsonHandler
   */
 
 object JSONHandler extends UtilT {
-  def main(args:Array[String]){
+  def main(args: Array[String]) {
     //Datetime
-    logger.info("{DATETIME: "+getTime+"}")
+    logger.info("{DATETIME: " + getTime + "}")
 
     var sep = "|"
     val argsList = argsParser(Map(), args.toList)
-    logger.info("{ARGUMENTS: "+argsList.toList.mkString(",")+"}")
+    logger.info("{ARGUMENTS: " + argsList.toList.mkString(",") + "}")
 
     //Elems in inputTestValidator
 
     val ns = (elem \\ "unit")
 
-    if(argsList.contains('Separator)){sep = getArgument(argsList,'Separator)}
+    if (argsList.contains('Separator)) {
+      sep = getArgument(argsList, 'Separator)
+    }
     logger.info("{LAUNCHING JSON PARSER USING " + sep + " AS SEPARATOR}")
 
-    testParamCheck(ns,argsList)
+    testParamCheck(ns, argsList)
 
-    argsList match {
-      case x: JSONHandler.ParserMap if !x.contains('InputFile) => {
-        usage
-        System.err.println("INPUT JSON FILE REQUIRED!")
-        System.exit(1)
-      }
-      case x: JSONHandler.ParserMap if x.contains('ObjectParser) => {
-          logger.info("{LAUNCHING JSON PARSER ON " + getArgument(x,'InputFile) + "USING " + getArgument(x,'ObjectParser) + "}")
-        parse(getArgument(x,'ObjectParser), getArgument(x,'InputFile))
-        log(getArgument(x,'ObjectParser))(getArgument(x,'ObjectParser))
-      }
+    if (!argsList.contains('InputFile)) {
+      usage
+      System.err.println("INPUT JSON FILE REQUIRED!")
+      System.exit(1)
+    }
+
+    if (argsList.contains('ObjectParser)){
+      logger.info("{LAUNCHING JSON PARSER ON " + getArgument(argsList, 'InputFile) + "USING " + getArgument(argsList, 'ObjectParser) + "}")
+      parse(getArgument(argsList, 'ObjectParser), getArgument(argsList, 'InputFile))
+      log(getArgument(argsList, 'ObjectParser))(getArgument(argsList, 'ObjectParser))
     }
   }
 }
+
 
