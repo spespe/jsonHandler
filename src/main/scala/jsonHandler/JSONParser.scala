45dab3305e848c727f6ce38ef59f97db427aeff4
Pietro.Speri <speri.pietro@gmail.com>
2018-04-18 18:00:01 -0700

    Added new type for Parser[String] in member

--- src/main/scala/jsonHandler/JSONParser.scala
+++ src/main/scala/jsonHandler/JSONParser.scala
@@ -11,28 +11,33 @@ import scala.util.parsing.input.Reader
 
 trait JSONParser extends JavaTokenParsers {
 
-  protected def multiple: Parser[List[Map[Any,Any]]] = rep(value).asInstanceOf[Parser[List[Map[Any,Any]]]]
+  protected def multiple: Parser[Any] = rep(value)
   protected def value: Parser[Any] = obj | arr | stringLiteral | floatingPointNumber ^^ (_.toDouble) |
-  "null" ^^  { _ => null }  | "true" ^^ { _ => true} | "false" ^^ { _ => false }
+    "null" ^^  { _ => null }  | "true" ^^ { _ => true} | "false" ^^ { _ => false }
   protected def obj: Parser[Map[String, Any]] = "{" ~> repsep(member, ",") <~ "}" ^^ (Map() ++ _)
   protected def arr: Parser[List[Any]] = "[" ~> repsep(value, ",") <~ "]"
-  protected def member: Parser[(String, Any)] = stringLiteral ~ ":" ~ value ^^ { case name ~ ":" ~ value => (name, value) }
+  protected def member: Parser[(String, String)] = stringLiteral ~ ":" ~ (value:Parser[String]) ^^ { case (name:String) ~ ":" ~ (value:String) => (name, value) }
 
   //Double conversion
-  protected def mapp:Parser[Any] = (element ~ "->").? ~ "Map(" ~ rep(list|mappList|mappEl|mapp|element) ~")" ~ (",").?
-  protected def mappList:Parser[Any] = element ~ "->" ~ list ~ (",").?
-  protected def mappEl:Parser[Any] = element ~ "->" ~ element ~ (",").?
-  protected def list: Parser[Any] = "List(" ~> repsep(element,",") <~ ")"
-  protected def element:Parser[Any] = "\""~" "|stringLiteral|floatingPointNumber~"\"" ~ (",").?
+  protected def mapp:Parser[Any] = (element <~ "->").? ~ "Map(" ~ rep(list|mappList|mappEl|mapp|element) ~ ")" <~ (",").?
+  protected def mappList:Parser[Any] = element <~ "->" ~> list ~ (",").?
+  protected def mappEl:Parser[Any] = element <~ "->" ~> element <~ (",").?
+  protected def map3:Parser[Any] = "Map(" ~> repsep(element~"->"~element,",") <~ ")"
+  protected def map2:Parser[Any] = "Map(" ~> element ~ "->" ~ list <~ ")" ^^ {case (element ~ "->" ~ element2) => Map(element->element2)}
+  protected def map1:Parser[Any] = "Map(" ~> element ~ "->" ~ element <~ ")" ^^ {case (element ~ "->" ~ element2) => Map(element->element2)}
+  protected def list: Parser[List[Any]] = "List(" ~> repsep(element,",") <~ ")" ^^ (_.toList)
+  protected def element:Parser[Any] = "\""~" "|stringLiteral|floatingPointNumber~"\"" ~ (",").? ^^ (_.toString)
 
   protected def parserLaunch(parser: Parser[Any], reader: Reader[Char]) = {
     parseAll(parser, reader) match {
-      case Success(matched:Traversable[_], _) => matched//matched.map(x=>println(findKeys(x, "title")))
+      case Success(matched, _) => matched//matched.asInstanceOf[List[Any]].map(x=>println(findKeys(x, "title")))
       case NoSuccess(noSuccMsg, _) => System.err.println("NO SUCCESS MESSAGE: " + noSuccMsg)
       case Failure(failMsg, _) => System.err.println("PLEASE CHECK THE INPUT JSON FILE. FAILURE: " + failMsg)
       case Error(errMsg, _) => System.err.println("PLEASE CHECK THE INPUT JSON FILE. ERROR: " + errMsg)
     }
   }
+
+  protected def parsedWriter()
 
   def findKeys(c: Any, key: Any): List[Any] = {
     @tailrec
